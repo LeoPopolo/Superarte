7ff315bc89967f27188fd43a8cee6ead31829392
LeoPopolo <leopopologuitarreta@gmail.com>
2025-03-25 20:23:03 -0300

    SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Superarte';
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('Instituto Superarte');
+    this.metaService.addTags([
+      { name: 'description', content: 'Formando adoradores' },
+      { name: 'keywords', content: 'instituto, educación, cursos, musica, arte, adoración, grabacion, audio, carrera, danza, pintura, guitarra, piano, canto, bateria, cristiano' },
+      { name: 'author', content: 'Instituto Superarte' },
+      { name: 'robots', content: 'index, follow' },
+      { 'http-equiv': 'content-language', content: 'es' },
+      { property: 'og:title', content: 'Instituto Superarte' },
+      { property: 'og:description', content: 'Formando adoradores' },
+      { property: 'og:image', content: '../assets/superarteLogo.png' },
+      { property: 'og:url', content: 'https://institutosuperarte.com.ar' }
+    ]);
+  }
 }
